4949052c832cf63f09e20ed2b11d9c8e52625a90
SergioBonatto <bonatto@tutanota.com>
2022-11-12 14:49:39 -0300

    aulas 6 ao 8

---
+++ 2. JavaScript Básico - Para iniciantes/aula6/index.js
@@ -0,0 +1,19 @@
+let nome 
+nome = "João";
+console.log(nome, "nasceu em 1980")
+nome = "Lucas"
+console.log(nome, "nasceu em 1990")
+
+// - Não podemos criar com palavras reservadas (if, else, const, etc)
+// - Variáveis precisam ter nomes significativos
+// - Nome da variável não pode começar com um número
+// - não pode conter espaço ou traço - (kebab-case)
+// - utilizamos camelCase
+// - ou snake_case
+// - é case-sensitive, nomeClient =/= nomeclient
+let nomeClient  = "Marcos"
+let nomeclient  = "Mateus"
+let nome_client = "Paulo"
+console.log (nomeClient, nomeclient, nome_client)
+
+// XXX: Não use VAR use LET!
---
+++ 2. JavaScript Básico - Para iniciantes/aula8/index.js
@@ -0,0 +1,23 @@
+const nome = "Cristovão"
+const sobrenome = "Colombo"
+const idade = 30
+let peso = 84
+const alturaEmMetros = 1.8
+let imc
+imc = peso / (alturaEmMetros **2)
+let anoNascimento
+anoNascimento = 2022 - idade
+
+console.log(nome, sobrenome + ", nasceu em ", anoNascimento, "e tem", idade, "anos, pesa", peso, "kg, tem", alturaEmMetros, "de altura e seu IMC é de", imc)
+
+console.log (`${nome} ${sobrenome} nasceu em ${anoNascimento}, e tem ${idade}, pesa ${peso}kg, tem ${alturaEmMetros} metros de altura e seu IMC é de ${imc} `)
+
+let meuPeso
+let minhaAltura
+let meuIMC
+
+meuPeso = 106
+minhaAltura = 1.85
+meuIMC = meuPeso / (minhaAltura ** 2)
+console.log(meuIMC)
+
