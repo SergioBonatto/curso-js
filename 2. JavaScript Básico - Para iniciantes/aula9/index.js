00e311d8d6a8d6e486afab70a541111dbddcbe88
SergioBonatto <bonatto@tutanota.com>
2022-11-12 15:23:15 -0300

    aula9

---
+++ 2. JavaScript Básico - Para iniciantes/aula9/index.js
@@ -0,0 +1,11 @@
+const nome          = 'fi';       // string  
+const nome1         = "Bonatto";  // string
+const nome2         = `tech`;     // string
+const num1          = 10;         // number
+const num2          = 10.52;      // number
+let nomeAluno;                    // undefined -> não aponta pra local nenhum na memória
+let sobrenomeAluno  = null;       // nulo -> não aponta pra local nenhum na memória
+const booleano      = true;       // boolean = true, false (lógico)
+let a = 2;
+let b = a
+console.log(typeof b)
